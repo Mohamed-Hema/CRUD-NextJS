730f1fccee4b284c67c38331d0193b31dea62304
Mohamed Hema <mohamedhema2017@gmail.com>
2024-01-04 15:59:29 +0200

    add Topic

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -1,22 +1,44 @@
 import RemoveBtn from "./RemoveBtn";
 import Link from "next/link";
 import { HiPencilAlt } from "react-icons/hi";
-export default function TopicsList() {
+
+const getTopics = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/topics", {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch topics");
+    }
+    return res.json();
+  } catch (error) {
+    console.log("Error Loading Topics: ", error);
+  }
+};
+
+export default async function TopicsList() {
+  const { topics } = await getTopics();
   return (
     <>
-      <div className="p-4 border-slate-300 my-3 flex justify-between gap-5 items-start">
-        <div>
-          <h2 className="font-bold text-2xl">Topic Title</h2>
-          <div>Topic Description</div>
-        </div>
+      {topics.map((t) => (
+        <div
+          key={t._id}
+          className="p-4 border-slate-300 my-3 flex justify-between gap-5 items-start"
+        >
+          <div>
+            <h2 className="font-bold text-2xl">{t.title}</h2>
+            <div>{t.description}</div>
+          </div>
 
-        <div className="flex gap-2">
-          <RemoveBtn />
-          <Link href={"/editTopic/123"}>
-            <HiPencilAlt size={24} />
-          </Link>
+          <div className="flex gap-2">
+            <RemoveBtn />
+            <Link href={`/editTopic/${t._id}`}>
+              <HiPencilAlt size={24} />
+            </Link>
+          </div>
         </div>
-      </div>
+      ))}
     </>
   );
 }
